d93da12a2f870c097029f630e05c1959436025fe
Aaron McDonald <sergeantbeast456@gmail.com>
2022-09-21 06:53:57 -0400

    initial functionality, program works

--- CPP_Find_the_cost_tiles/CPP_Find_the_cost_tiles.cpp
+++ CPP_Find_the_cost_tiles/CPP_Find_the_cost_tiles.cpp
@@ -1,20 +1,26 @@
-// CPP_Find_the_cost_tiles.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// C++ Find the cost of tiles for an area
+// This program will calculate the cost for tiles over a certain area based on the user input
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+	double length;
+	double width;
+	double cost_per_unit;
+	double total_area;
+
+	cout << "Please enter the width: ";
+	cin >> width;
+
+	cout << "Please enter the length: ";
+	cin >> length;
+
+	cout << "Please enter the cost per unit: ";
+	cin >> cost_per_unit;
+
+	total_area = width * length;
+	cout << "The total cost of tiles for this area is " << cost_per_unit * total_area << endl;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
